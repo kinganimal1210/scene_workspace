31a27847013d9b678bf5884c65c113e426432697
kinganimal1210 <kinganimal@naver.com>
2025-03-26 18:48:27 +0900

    0326 파일구조수정 오민규

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function TodoList() {
+    return (
+      <ul style={{
+        backgroundColor: 'black',
+        color: 'pink'
+      }}>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    );
+  }
+  
 
-export default App;
+
